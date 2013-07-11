faf98862fc9fbd6ab08658c955b140e0f247e7e1
Donald Ho <donald.ho@eloqua.com>
2013-07-11 11:20:50 -0400

    Re-adding Postal dlls

---
+++ Concierge/Controllers/EmailController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Postal;
+
+namespace Concierge.Controllers
+{
+    public class EmailController : Controller
+    {
+        //
+        // GET: /Email/
+
+        //public ActionResult Index()
+        //{
+        //    return View();
+        //}
+
+        public ActionResult Profile()
+        {
+            dynamic email = new Email("Profile");
+            return View();
+        }
+    }
+}
