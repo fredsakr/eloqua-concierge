961378b844c0ab3dedb96768a1d9b2e99e886fbf
Fred Sakr <fred.sakr@eloqua.com>
2013-07-11 13:28:25 -0400

    disable input validation

--- Concierge/Controllers/ConciergeRequestController.cs
+++ Concierge/Controllers/ConciergeRequestController.cs
@@ -1,4 +1,5 @@
-﻿using System.Data.SqlClient;
+﻿using System;
+using System.Data.SqlClient;
 using System.Web.Mvc;
 using Concierge.Infrastructure;
 
@@ -16,27 +17,21 @@ namespace Concierge.Controllers
             string fromName = string.Empty;
             string subject = string.Empty;
 
-            if (!string.IsNullOrEmpty(Request.Params["envelope"]))
-            {
-                fromAddress = Request.Params["envelope"];
-            }
-
-            if (!string.IsNullOrEmpty(Request.Params["headers"]))
-            {
-                subject = Request.Params["headers"];
-            }
-
             if (!string.IsNullOrEmpty(Request.Params["From"]))
             {
                 fromName = Request.Params["From"];
             }
 
+            if (!string.IsNullOrEmpty(Request.Headers["subject"]))
+            {
+                subject = Request.Params["subject"];
+            }
+
             try
             {
                 myConnection.Open();
-                SqlCommand myCommand = new SqlCommand(string.Format("INSERT INTO Request (RequestId, FromAddress, FromName, Subject) VALUES (1, '{0}', '{1}', '{2}')", fromAddress, fromName, subject), myConnection);
+                SqlCommand myCommand = new SqlCommand(string.Format("INSERT INTO Request (RequestId, FromAddress, FromName, Subject, CreateDate) VALUES (1, '{0}', '{1}', '{2}', '{3}')", fromAddress, fromName, subject, DateTime.Now), myConnection);
                 myCommand.ExecuteNonQuery();
-
             }
             finally
             {
