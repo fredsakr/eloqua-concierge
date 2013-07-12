c27ea8da085b4651c0c53d19db53836fd197872c
Donald Ho <donald.ho@eloqua.com>
2013-07-11 21:07:18 -0400

    Hardcoding image references for email

--- Concierge/Models/Activity.cs
+++ Concierge/Models/Activity.cs
@@ -12,15 +12,15 @@ namespace Concierge.Models
         {
             if (type == Eloqua.Api.Rest.ClientLibrary.Models.Data.Activities.ActivityType.emailOpen.ToString())
             {
-                return "list-style-type: none; background: url('/images/email-s.png') no-repeat; padding-left:32px; height: 32px;";
+                return "list-style-type: none; background: url('http://eloqua-concierge.apphb.com/images/email-s.png') no-repeat; padding-left:32px; height: 32px;";
             }
             else if (type == Eloqua.Api.Rest.ClientLibrary.Models.Data.Activities.ActivityType.formSubmit.ToString())
             {
-                return "list-style-type: none; background: url('/images/form-s.png') no-repeat; padding-left:32px; height: 32px;";
+                return "list-style-type: none; background: url('http://eloqua-concierge.apphb.com/images/form-s.png') no-repeat; padding-left:32px; height: 32px;";
             }
             else if (type == Eloqua.Api.Rest.ClientLibrary.Models.Data.Activities.ActivityType.webVisit.ToString())
             {
-                return "list-style-type: none; background: url('/images/website-s.png') no-repeat; padding-left:32px; height: 32px;";
+                return "list-style-type: none; background: url('http://eloqua-concierge.apphb.com/images/website-s.png') no-repeat; padding-left:32px; height: 32px;";
             }
             return "list-style-type: none; no-repeat; padding-left:32px; height: 32px;";
         }
