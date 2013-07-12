45a7da5948f1d5398425f4500f0d2b7ae19bd565
Fred Sakr <fred.sakr@eloqua.com>
2013-07-12 09:14:13 -0400

    adding endpoints for email links

---
+++ Concierge/Controllers/SendEmailController.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Mvc;
+
+namespace Concierge.Controllers
+{
+    public class SendEmailController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
